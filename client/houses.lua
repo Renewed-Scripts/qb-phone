1a3c33a3686814787adc24348a6f8d5c2b5385bf
FjamZoo <lauridspedersen83@gmail.com>
2022-05-30 13:23:27 +0200

    Forgot to push the house edit 😒

--- client/houses.lua
+++ client/houses.lua
@@ -2,13 +2,13 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 -- NUI Callback
 
-RegisterNUICallback('GetPlayerHouses', function(data, cb)
+RegisterNUICallback('GetPlayerHouses', function(_, cb)
     QBCore.Functions.TriggerCallback('qb-phone:server:GetPlayerHouses', function(Houses)
         cb(Houses)
     end)
 end)
 
-RegisterNUICallback('GetPlayerKeys', function(data, cb)
+RegisterNUICallback('GetPlayerKeys', function(_, cb)
     QBCore.Functions.TriggerCallback('qb-phone:server:GetHouseKeys', function(Keys)
         cb(Keys)
     end)
@@ -17,14 +17,17 @@ end)
 RegisterNUICallback('SetHouseLocation', function(data, cb)
     SetNewWaypoint(data.HouseData.HouseData.coords.enter.x, data.HouseData.HouseData.coords.enter.y)
     QBCore.Functions.Notify("GPS set to " .. data.HouseData.HouseData.adress .. "!", "success")
+    cb("ok")
 end)
 
-RegisterNUICallback('RemoveKeyholder', function(data)
+RegisterNUICallback('RemoveKeyholder', function(data, cb)
     TriggerServerEvent('qb-houses:server:removeHouseKey', data.HouseData.name, {
         citizenid = data.HolderData.citizenid,
         firstname = data.HolderData.charinfo.firstname,
         lastname = data.HolderData.charinfo.lastname,
     })
+
+    cb("ok")
 end)
 
 RegisterNUICallback('TransferCid', function(data, cb)
