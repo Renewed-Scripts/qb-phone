2143f39d6d262f4b93133aea4d2434ae370c7c5b
FjamZoo <lauridspedersen83@gmail.com>
2022-06-04 17:51:30 +0200

    Removed bill command as its replaced with debt app

--- server/commands.lua
+++ server/commands.lua
@@ -14,35 +14,6 @@ QBCore.Commands.Add("setmetadata", "Set Player Metadata (God Only)", {}, false, 
     end
 end, "god")
 
-QBCore.Commands.Add('bill', 'Bill A Player', {{name = 'id', help = 'Player ID'}, {name = 'amount', help = 'Fine Amount'}}, false, function(source, args)
-    local biller = QBCore.Functions.GetPlayer(source)
-    local billed = QBCore.Functions.GetPlayer(tonumber(args[1]))
-    local amount = tonumber(args[2])
-    if biller.PlayerData.job.name == "police" or biller.PlayerData.job.name == 'ambulance' or biller.PlayerData.job.name == 'mechanic' then
-        if billed then
-            if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
-                if amount and amount > 0 then
-                    exports.oxmysql:insert(
-                        'INSERT INTO phone_invoices (citizenid, amount, society, sender, sendercitizenid) VALUES (?, ?, ?, ?, ?)',
-                        {billed.PlayerData.citizenid, amount, biller.PlayerData.job.name,
-                         biller.PlayerData.charinfo.firstname, biller.PlayerData.citizenid})
-                    TriggerClientEvent('qb-phone:RefreshPhone', billed.PlayerData.source)
-                    TriggerClientEvent('QBCore:Notify', source, 'Invoice successfully sent')
-                    TriggerClientEvent('QBCore:Notify', billed.PlayerData.source, 'New Invoice Received')
-                else
-                    TriggerClientEvent('QBCore:Notify', source, 'Must be a valid amount above 0', 'error')
-                end
-            else
-                TriggerClientEvent('QBCore:Notify', source, 'You cannot bill yourself...', 'error')
-            end
-        else
-            TriggerClientEvent('QBCore:Notify', source, 'Player not Online', 'error')
-        end
-    else
-        TriggerClientEvent('QBCore:Notify', source, 'No Access', 'error')
-    end
-end)
-
 QBCore.Commands.Add("p#", "Provide Phone Number", {}, false, function(source, args)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
