e14e4f4467ae6431583b43611949a660aab57916
iLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2022-06-22 19:43:05 +0200

    Reverting it last time

--- config.lua
+++ config.lua
@@ -394,7 +394,7 @@ Config.JobCenter = {
 Config.ServiceJobs = {
     {
         Job = "lawyer",
-        HeaderBackgroundColor = "rgb(42, 137, 215)"
+        HeaderBackgroundColor = "rgb(42, 137, 214)"
     },
     {
         Job = "realestate",
