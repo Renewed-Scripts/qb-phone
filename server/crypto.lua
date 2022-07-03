dba17f9420fd3782cff6f1bb9f7e9cf9a92d2909
FjamZoo <lauridspedersen83@gmail.com>
2022-07-04 01:02:10 +0200

    (CRYPTO) More Security and Notifications

--- server/crypto.lua
+++ server/crypto.lua
@@ -3,7 +3,10 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 
 -- exports['qb-phone']:RemoveCrypto(Player, type, amount)
-local function RemoveCrypto(Player, type, amount)
+local function RemoveCrypto(src, type, amount)
+    if not src then return end
+    local Player = QBCore.Functions.GetPlayer(src)
+
     if not Player or not type or not amount then return end
 
     local Crypto = Player.PlayerData.metadata[type]
@@ -15,7 +18,10 @@ end exports("RemoveCrypto", RemoveCrypto)
 
 
 -- exports['qb-phone']:AddCrypto(Player, type, amount)
-local function AddCrypto(Player, type, amount)
+local function AddCrypto(src, type, amount)
+    if not src then return end
+    local Player = QBCore.Functions.GetPlayer(src)
+
     if not Player or not type or not amount then return end
 
     local Crypto = Player.PlayerData.metadata[type]
@@ -26,7 +32,7 @@ local function AddCrypto(Player, type, amount)
 end exports("AddCrypto", AddCrypto)
 
 local function GetConfig(metadata)
-    for k, v in pairs(Config.CryptoCoins) do
+    for _, v in pairs(Config.CryptoCoins) do
         if v.metadata == metadata then
             return v
         end
@@ -36,13 +42,28 @@ end
 RegisterNetEvent('qb-phone:server:PurchaseCrypto', function(type, amount)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    if not Player.PlayerData.metadata[type] then return end -- if the crypto dosnt exist
+    if not Player or not Player.PlayerData.metadata[type] then return end -- if the crypto dosnt exist
 
     local v = Config.CryptoCoins[GetConfig(type)]
     local cashAmount = amount * v.value
 
     if Player.PlayerData.money.bank and Player.PlayerData.money.bank >= cashAmount then
         Player.Functions.RemoveMoney('bank', cashAmount, "Crypto Purchased: "..v.abbrev)
-        AddCrypto(Player, type, amount)
+        TriggerClientEvent('qb-phone:client:CustomNotification', src,
+            "Purchased Crypto",
+            "You Purchased "..amount.." "..type.." Crypto",
+            "fas fa-check-circle",
+            "#0074FF",
+            7500
+        )
+        AddCrypto(src, type, amount)
+    else
+        TriggerClientEvent('qb-phone:client:CustomNotification', src,
+            "Error",
+            "Not Enough Money",
+            "fas fa-dollar-sign",
+            "#FF0000",
+            7500
+        )
     end
 end)
