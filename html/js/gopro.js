59527bdaa9dd842c0aa75f60c529416163cf0ce5
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-08-04 04:11:22 -0400

    :( Nothing Here!

--- html/js/gopro.js
+++ html/js/gopro.js
@@ -15,11 +15,13 @@ function ConfirmationFrame() {
 
 SetupGoPros = function(cams) {
     $(".gopro-lists").html("");
-    if (cams != null) {
+    if (JSON.stringify(cams) != "[]"){
         $.each(cams, function(i, cams){
             var Element = '<div class="gopro-list" id="vehicle-'+i+'"> <div class="gopro-list-icon"><i class="fas fa-camera"></i></div> <div class="gopro-list-name">'+cams.name+'</div> <div class="gopro-action-buttons"> <i class="fas fa-eye" id="gopro-view-camera" data-id="'+cams.id+'" data-toggle="tooltip" title="View"></i> <i class="fas fa-thumbtack" id="gopro-track-camera" data-id="'+cams.id+'" data-toggle="tooltip" title="Track"></i> <i class="fas fa-user-plus" id="gopro-addto-camera" data-id="'+cams.id+'" data-toggle="tooltip" title="Add Someone"></i>  </div></div>';
             $(".gopro-lists").append(Element);
         });
+    }else{
+        $(".gopro-lists").html('<p class="nomails">Nothing Here! <i class="fas fa-frown" id="mail-frown"></i></p>');
     }
 }
 
