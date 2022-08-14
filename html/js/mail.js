4862df7faae10afebb7602d8498d280d67b6eab1
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-08-14 03:57:19 -0400

    Mail app redesign/ cleanup/ optimization

--- html/js/mail.js
+++ html/js/mail.js
@@ -1,13 +1,8 @@
 var OpenedMail = null;
 
-$(document).ready(function(){
-    $("#advert-search").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $(".advert").filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-        });
-    });
-});
+// Mail APP
+
+// Search
 
 $(document).ready(function(){
     $("#mail-search").on("keyup", function() {
@@ -18,89 +13,42 @@ $(document).ready(function(){
     });
 });
 
-$(document).on('click', '.mail', function(e){
-    e.preventDefault();
-
-    $(".mail-home").animate({
-        left: 30+"vh"
-    }, 300);
-    $(".opened-mail").animate({
-        left: 0+"vh"
-    }, 300);
-
-    var MailData = $("#"+$(this).attr('id')).data('MailData');
-    QB.Phone.Functions.SetupMail(MailData);
-
-    OpenedMail = $(this).attr('id');
-});
-
-$(document).on('click', '.mail-back', function(e){
-    e.preventDefault();
-
-    $(".mail-home").animate({
-        left: 0+"vh"
-    }, 300);
-    $(".opened-mail").animate({
-        left: -30+"vh"
-    }, 300);
-    OpenedMail = null;
-});
-
-$(document).on('click', '#accept-mail', function(e){
-    e.preventDefault();
-    var MailData = $("#"+OpenedMail).data('MailData');
-    $.post('https://qb-phone/AcceptMailButton', JSON.stringify({
-        buttonEvent: MailData.button.buttonEvent,
-        buttonData: MailData.button.buttonData,
-        mailId: MailData.mailid,
-    }));
-    $(".mail-home").animate({
-        left: 0+"vh"
-    }, 300);
-    $(".opened-mail").animate({
-        left: -30+"vh"
-    }, 300);
-});
-
-$(document).on('click', '#remove-mail', function(e){
-    e.preventDefault();
-    var MailData = $("#"+OpenedMail).data('MailData');
-    $.post('https://qb-phone/RemoveMail', JSON.stringify({
-        mailId: MailData.mailid
-    }));
-    $(".mail-home").animate({
-        left: 0+"vh"
-    }, 300);
-    $(".opened-mail").animate({
-        left: -30+"vh"
-    }, 300);
-});
+// Functions
 
 QB.Phone.Functions.SetupMails = function(Mails) {
-    var NewDate = new Date();
-    var NewHour = NewDate.getHours();
-    var NewMinute = NewDate.getMinutes();
-    var Minutessss = NewMinute;
-    var Hourssssss = NewHour;
-    if (NewHour < 10) {
-        Hourssssss = "0" + Hourssssss;
-    }
-    if (NewMinute < 10) {
-        Minutessss = "0" + NewMinute;
-    }
-    var MessageTime = Hourssssss + ":" + Minutessss;
-
-    $("#mail-header-mail").html(QB.Phone.Data.PlayerData.charinfo.firstname+"."+QB.Phone.Data.PlayerData.charinfo.lastname+"@brazzers.cum");
     if (Mails !== null && Mails !== undefined) {
         if (Mails.length > 0) {
             $(".mail-list").html("");
             $.each(Mails, function(i, mail){
-                var date = new Date(mail.date);
-                var DateString = date.getDay()+" "+MonthFormatting[date.getMonth()]+" "+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes();
-                var element = '<div class="mail" id="mail-'+mail.mailid+'"><span class="mail-sender" style="font-weight: bold;">'+mail.sender+'</span> <div class="mail-text"><p>'+mail.message+'</p></div> <div class="mail-time">'+DateString+'</div></div>';
+                var TimeAgo = moment(mail.date).format('MM/DD/YYYY hh:mm');
 
-                $(".mail-list").append(element);
-                $("#mail-"+mail.mailid).data('MailData', mail);
+                if (JSON.stringify(mail.button) != "[]"){
+                    var element = '<div class="mail" id="mail-'+mail.mailid+'" data-mailid="'+mail.mailid +'">'+ 
+                        '<span class="mail-sender">From: '+mail.sender+'</span>' +
+                            '<div class="mail-subject"><p>Subject: '+mail.subject+'</p></div>' +
+                            '<div class="mail-block">' +
+                                '<div class="mail-message">'+mail.message+'</div>' +
+                                '<div class="mail-box"><span class="mail-box mail-accept" style="margin-left: 4.0vh;">ACCEPT</span><span class="mail-box mail-delete" style = "margin-left: 1.1vh;">DELETE</span></div>' +
+                            '</div>' +
+                            '<div class="mail-line"></div>' +
+                            '<div class="mail-time">'+TimeAgo+'</div>' + 
+                        '</div>';
+                        $(".mail-list").append(element);
+                        $("#mail-"+mail.mailid).data('MailData', mail);
+                }else{
+                    var element = '<div class="mail" id="mail-'+mail.mailid+'" data-mailid="'+mail.mailid +'">'+ 
+                        '<span class="mail-sender">From: '+mail.sender+'</span>' +
+                            '<div class="mail-subject"><p>Subject: '+mail.subject+'</p></div>' +
+                            '<div class="mail-block">' +
+                                '<div class="mail-message">'+mail.message+'</div>' +
+                                '<div class="mail-box"><span class="mail-box mail-delete" style = "margin-left: 7.0vh;">DELETE</span></div>' +
+                            '</div>' +
+                            '<div class="mail-line"></div>' +
+                            '<div class="mail-time">'+TimeAgo+'</div>' + 
+                        '</div>';
+                    $(".mail-list").append(element);
+                    $("#mail-"+mail.mailid).data('MailData', mail);
+                }
             });
         } else {
             $(".mail-list").html('<p class="nomails">Nothing Here! <i class="fas fa-frown" id="mail-frown"></i></p>');
@@ -109,31 +57,43 @@ QB.Phone.Functions.SetupMails = function(Mails) {
     }
 }
 
-var MonthFormatting = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// Clicks
 
-QB.Phone.Functions.SetupMail = function(MailData) {
-    var date = new Date(MailData.date);
-    var DateString = date.getDay()+" "+MonthFormatting[date.getMonth()]+" "+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes();
-    $(".mail-subject").html("<p><span style='font-weight: bold;'>"+MailData.sender+"</span><br>"+MailData.subject+"</p>");
-    $(".mail-date").html("<p>"+DateString+"</p>");
-    $(".mail-content").html("<p>"+MailData.message+"</p>");
+$(document).on('click', '.mail-accept', function(e){
+    e.preventDefault();
+    var mailId = $(this).parent().parent().parent().data('mailid');
+    var MailData = $("#"+OpenedMail).data('MailData');
+    $.post('https://qb-phone/AcceptMailButton', JSON.stringify({
+        buttonEvent: MailData.button.buttonEvent,
+        buttonData: MailData.button.buttonData,
+        mailId: mailId,
+    }));
+});
 
-    var AcceptElem = '<div class="opened-mail-footer-item" id="accept-mail"><i class="fas fa-check-circle mail-icon"></i></div>';
-    var RemoveElem = '<div class="opened-mail-footer-item" id="remove-mail"><i class="fas fa-trash-alt mail-icon"></i></div>';
+$(document).on('click', '.mail-delete', function(e){
+    e.preventDefault();
+    var mailId = $(this).parent().parent().parent().data('mailid');
+    $.post('https://qb-phone/RemoveMail', JSON.stringify({
+        mailId: mailId
+    }));
+});
 
-    $(".opened-mail-footer").html("");
-
-    if (MailData.button !== undefined && MailData.button !== null) {
-        $(".opened-mail-footer").append(AcceptElem);
-        $(".opened-mail-footer").append(RemoveElem);
-        $(".opened-mail-footer-item").css({"width":"50%"});
-    } else {
-        $(".opened-mail-footer").append(RemoveElem);
-        $(".opened-mail-footer-item").css({"width":"100%"});
-    }
-}
+$(document).on('click', '.mail', function(e){
+    e.preventDefault();
+    $(this).find(".mail-block").toggle();
+    OpenedMail = $(this).attr('id');
+});
 
 // Advert JS
+
+$(document).ready(function(){
+    $("#advert-search").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $(".advert").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        });
+    });
+});
 
 // Functions
 
