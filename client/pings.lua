c0f4bd853b65a8c7268af6102fb4de52f7ce334a
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-08-16 17:39:10 -0400

    Cleaned up/ Modified Ping App UI

--- client/pings.lua
+++ client/pings.lua
@@ -21,18 +21,6 @@ RegisterNetEvent("qb-phone:client:sendPing", function(Name, pos)
     end)
 end)
 
-RegisterNUICallback('AcceptPingPlayer', function(_, cb)
-    TriggerServerEvent('qb-pings:server:acceptping')
-    TriggerEvent("qb-phone:ping:client:UiUppers", false)
-    cb('ok')
-end)
-
-RegisterNUICallback('rejectPingPlayer', function(_, cb)
-    TriggerServerEvent('qb-pings:server:denyping')
-    TriggerEvent("qb-phone:ping:client:UiUppers", false)
-    cb('ok')
-end)
-
 RegisterNUICallback('SendPingPlayer', function(data, cb)
     TriggerServerEvent('qb-phone:server:sendPing', data.id)
     cb('ok')
--- html/js/ping.js
+++ html/js/ping.js
@@ -1,35 +1,10 @@
 $(document).on('click', '#ping-send', function(e){
     e.preventDefault();
-    var IDPlayer = $("#channel").val();
+    var IDPlayer = $(".ierplol").val();
     if (IDPlayer >= 1){
         $.post('https://qb-phone/SendPingPlayer', JSON.stringify({
             id: IDPlayer
         }));
-        $("#channel").val("");
+        $(".ierplol").val("");
     }
-});
-
-$(document).on('click', '#ping-accept', function(e){
-    e.preventDefault();
-    $.post('https://qb-phone/AcceptPingPlayer', JSON.stringify({}));
-});
-
-$(document).on('click', '#ping-reject', function(e){
-    e.preventDefault();
-    $.post('https://qb-phone/rejectPingPlayer', JSON.stringify({}));
-});
-
-$(document).ready(function(){
-    window.addEventListener('message', function(event) {
-        switch(event.data.action) {
-            case "acceptrejectBlock":
-                $("#ping-accept").css({"display":"block"});
-                $("#ping-reject").css({"display":"block"});
-                break;
-            case "acceptrejectNone":
-                $("#ping-accept").css({"display":"none"});
-                $("#ping-reject").css({"display":"none"});
-                break;  
-        }
-    })
 });
