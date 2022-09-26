368b65e41ae88f923ea16f3903fb6ff45583c656
Peroxcide <martinmenegazzi@gmail.com>
2022-09-26 18:41:18 -0300

    documents cleanup

--- html/js/documents.js
+++ html/js/documents.js
@@ -18,18 +18,11 @@ $(document).ready(function(){
     window.addEventListener('message', function(event) {
         switch(event.data.action) {
             case "DocumentRefresh":
-                LoadGetNotes()
+                getDocuments();
             break;
-        }
-    })
-});
-
-$(document).ready(function(){
-    window.addEventListener('message', function(event) {
-        switch(event.data.action) {
             case "DocumentSent":
                 SendDocument(event.data.DocumentSend.title, event.data.DocumentSend.text);
-                break;
+            break;
         }
     })
 });
@@ -62,6 +55,21 @@ function MainMenu(){
     }
 }
 
+function getDocuments(){
+    $(this).parents('.documents-dropdown').find('span').text($(this).text());
+    $(this).parents('.documents-dropdown').find('input').attr('value', $(this).attr('id'));
+    $(".documents-list").html(""); // Frown Face before loading any contents if any!
+        var AddOption = '<div class="casino-text-clear">Nothing Here!</div>'+
+        '<div class="casino-text-clear" style="font-size: 500%;color: #FFFFFF;"><i class="fas fa-frown"></i></div>'
+    $('.documents-list').append(AddOption);
+
+    $.post('https://qb-phone/GetNote_for_Documents_app', JSON.stringify({}), function(HasNote){
+        if(HasNote){
+            AddDocuments(HasNote)
+        }
+    });
+}
+
 function AddDocuments(data){
     $(".documents-list").html("");
 
@@ -73,7 +81,7 @@ function AddDocuments(data){
     for (const [k, v] of Object.entries(data)) {
         var firstLetter = v.title.substring(0, 1);  
         var Fulltext = firstLetter.toUpperCase()+(v.title).replace(firstLetter,'')
-
+        
         var AddOption = '<div class="documents-test">' + 
             '<div class="documents-title-title">'+Fulltext+'</div>' +
             '<div class="documents-title-icon" data-title="'+v.title+'" data-text="'+v.text+'" data-id="'+v.id+'" data-csn="'+v.citizenid+'"><i class="fas fa-eye"></i></div>'+
@@ -85,7 +93,6 @@ function AddDocuments(data){
 
 function LoadGetNotes(){
     $(".documents-dropdown-menu").html("");
-
     var Shitter = '<li id="documents-docs" data-title="Documents">Documents' +
         '<li id="documents-licenses" data-title="Licenses">Licenses</li>' +
         '<li id="documents-vehicle" data-title="Vehicle">Vehicle Registrations</li>' +
@@ -94,7 +101,7 @@ function LoadGetNotes(){
     $('.documents-dropdown-menu').append(Shitter);
 }
 
-SendDocument = function(title, text) {
+function SendDocument(title, text){
     MainMenu()
     $(".documents-list").html("");
 
@@ -111,83 +118,27 @@ SendDocument = function(title, text) {
     DocEndid = $(this).data('id')
     DocEndcitizenid = $(this).data('csn')
 
-    var AddOption = '<div class="document-body-class-body-main">'+
-                        '<div id="documents-textarea-new" spellcheck="false" required placeholder="Text" maxlength="4000">'+DocEndtext+'</div>'+
-                    '</div>';
+    var AddOption = `
+    <div class="document-body-class-body-main">'+
+        <div id="documents-textarea-new" spellcheck="false" required placeholder="Text" maxlength="4000">${DocEndtext}</div>
+    </div>`;
 
-    var AnotherOption = '<div class="document-body-class-body-main">'+
-                            '<div class="documents-input-title-list">Title</div>'+
-                            '<div class="documents-input-title-name">'+DocEndtitle+'</div>'+
-                            '<div class="documents-input-tags"><i class="fas fa-tags"></i></div>'+
-                            '<div class="documents-input-back"><i class="fas fa-chevron-left"></i></div>'+
-                        '</div>';
+    var AnotherOption = `
+    <div class="document-body-class-body-main">
+        <div class="documents-input-title-list">Title</div>
+        <div class="documents-input-title-name">${DocEndtitle}</div>
+        <div class="documents-input-tags"><i class="fas fa-tags"></i></div>
+        <div class="documents-input-back"><i class="fas fa-chevron-left"></i></div>
+    </div>`;
 
     $('.documents-list').append(AddOption);
     $('.documents-header').append(AnotherOption);
 }
 
-// Clicks 
-
-$(document).on('click', '.documents-tupe-text-btn', function(e){
-    e.preventDefault();
-    ClearInputNew()
-    $('#documents-box-new-add-new').fadeIn(350);
-});
-
-$(document).on('click', '#documents-send-perm', function(e){
-    e.preventDefault();
-    var date = new Date();
-    var Times = date.getDay()+" "+MonthFormatting[date.getMonth()]+" "+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes();
-    var StateID = $(".documents-input-stateid").val();
-    var NewText = $("#documents-textarea-new").val();
-    if(NewText != ""){
-        $.post('https://qb-phone/document_Send_Note', JSON.stringify({
-            Title: DocEndtitle,
-            Text: NewText,
-            Time: Times,
-            ID: DocEndid,
-            CSN: DocEndcitizenid,
-            StateID: StateID,
-            Type: "PermSend",
-        }));
-    }
-    ClearInputNew()
-    $('#documents-send-stateid').fadeOut(350);
-});
-
-$(document).on('click', '#documents-save-note-for-doc', function(e){
-    e.preventDefault();
-    var Title = $(".documents-input-title").val();
-    var Text = $("#documents-textarea").val();
-    var date = new Date();
-    var Times = date.getDay()+" "+MonthFormatting[date.getMonth()]+" "+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes();
-
-    if ((Title &&Text ) != ""){
-        $.post('https://qb-phone/documents_Save_Note_As', JSON.stringify({
-            Title: Title,
-            Text: Text,
-            Time: Times,
-            Type: "New",
-        }));
-        ClearInputNew()
-        $("#documents-textarea").val("");
-        $('#documents-box-new-add-new').fadeOut(350);
-    }
-});
+// Clicks
 
 $(document).on('click', '#documents-docs', function(e) {
-    $(this).parents('.documents-dropdown').find('span').text($(this).text());
-    $(this).parents('.documents-dropdown').find('input').attr('value', $(this).attr('id'));
-    $(".documents-list").html(""); // Frown Face before loading any contents if any!
-        var AddOption = '<div class="casino-text-clear">Nothing Here!</div>'+
-        '<div class="casino-text-clear" style="font-size: 500%;color: #FFFFFF;"><i class="fas fa-frown"></i></div>'
-    $('.documents-list').append(AddOption);
-
-    $.post('https://qb-phone/GetNote_for_Documents_app', JSON.stringify({}), function(HasNote){
-        if(HasNote){
-            AddDocuments(HasNote)
-        }
-    });
+    getDocuments();
 });
 
 $(document).on('click', '#documents-vehicle', function(e) {
@@ -198,7 +149,6 @@ $(document).on('click', '#documents-vehicle', function(e) {
         if(Vehicles != null){
             $.each(Vehicles, function(i, vehicle){
                 if (vehicle.vinscratched != 'false'){
-                    if (vehicle.vinscratched == 0){
                         DocEndtitle = null
                         DocEndtext = null
                         DocEndid = null
@@ -206,30 +156,16 @@ $(document).on('click', '#documents-vehicle', function(e) {
         
                         var firstLetter = vehicle.fullname.substring(0, 1);  
                         var Fulltext = firstLetter.toUpperCase()+(vehicle.fullname).replace(firstLetter,'')
+                        var FirstName = QB.Phone.Data.PlayerData.charinfo.firstname;
+                        var LastName = QB.Phone.Data.PlayerData.charinfo.lastname;
                 
                         var AddOption = '<div class="documents-test">' + 
                             '<div class="documents-title-title">'+Fulltext+'</div>' +
-                            '<div class="documents-title-icon-registration" data-title="'+vehicle.fullname+'" data-text="<b><center><u>San Andreas DMV</u></b></center><p><p><b>Name: </b>'+vehicle.brand+'</p></p><p><b>Model: </b>'+vehicle.model+'</p><p><b>Plate: </b>'+vehicle.plate+'</p><p><b><center>Official State Document Of San Andreas</p></b></center>"><i class="fas fa-eye"></i></div>'+
+                            '<div class="documents-title-icon-registration" data-title="'+vehicle.fullname+'" data-text="<b><center><u>San Andreas DMV</u></b></center><p><p><b>Name: </b>'+vehicle.brand+'</p></p><p><b>Model: </b>'+vehicle.model+'</p><p><b>Plate: </b>'+vehicle.plate+'</p><p><b>Owner: </b>'+FirstName+' '+LastName+'</p><p><b><center>Official State Document Of San Andreas</p></b></center>"><i class="fas fa-eye"></i></div>'+
                         '</div>';
                 
                         $('.documents-list').append(AddOption);
                     }
-                } else {
-                    DocEndtitle = null
-                    DocEndtext = null
-                    DocEndid = null
-                    DocEndcitizenid = null
-    
-                    var firstLetter = vehicle.fullname.substring(0, 1);  
-                    var Fulltext = firstLetter.toUpperCase()+(vehicle.fullname).replace(firstLetter,'')
-            
-                    var AddOption = '<div class="documents-test">' + 
-                        '<div class="documents-title-title">'+Fulltext+'</div>' +
-                        '<div class="documents-title-icon-registration" data-title="'+vehicle.fullname+'" data-text="<b><center><u>San Andreas DMV</u></b></center><p><p><b>Name: </b>'+vehicle.brand+'</p></p><p><b>Model: </b>'+vehicle.model+'</p><p><b>Plate: </b>'+vehicle.plate+'</p><p><b><center>Official Document Of San Andreas</p></b></center>"><i class="fas fa-eye"></i></div>'+
-                    '</div>';
-            
-                    $('.documents-list').append(AddOption);
-                }
             });
         } else {
             var AddOption = '<div class="casino-text-clear">Nothing Here!</div>'+
@@ -276,6 +212,53 @@ $(document).on('click', '#documents-licenses', function(e) {
     }
 });
 
+$(document).on('click', '.documents-tupe-text-btn', function(e){
+    e.preventDefault();
+    ClearInputNew()
+    $('#documents-box-new-add-new').fadeIn(350);
+});
+
+$(document).on('click', '#documents-send-perm', function(e){
+    e.preventDefault();
+    var date = new Date();
+    var Times = date.getDay()+" "+MonthFormatting[date.getMonth()]+" "+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes();
+    var StateID = $(".documents-input-stateid").val();
+    var NewText = $("#documents-textarea-new").val();
+    if(NewText != ""){
+        $.post('https://qb-phone/document_Send_Note', JSON.stringify({
+            Title: DocEndtitle,
+            Text: NewText,
+            Time: Times,
+            ID: DocEndid,
+            CSN: DocEndcitizenid,
+            StateID: StateID,
+            Type: "PermSend",
+        }));
+    }
+    ClearInputNew()
+    $('#documents-send-stateid').fadeOut(350);
+});
+
+$(document).on('click', '#documents-save-note-for-doc', function(e){
+    e.preventDefault();
+    var Title = $(".documents-input-title").val();
+    var Text = $("#documents-textarea").val();
+    var date = new Date();
+    var Times = date.getDay()+" "+MonthFormatting[date.getMonth()]+" "+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes();
+
+    if ((Title && Text) != ""){
+        $.post('https://qb-phone/documents_Save_Note_As', JSON.stringify({
+            Title: Title,
+            Text: Text,
+            Time: Times,
+            Type: "New",
+        }));
+        ClearInputNew()
+        $("#documents-textarea").val("");
+        $('#documents-box-new-add-new').fadeOut(350);
+    }
+});
+
 $(document).on('click', '.documents-title-icon-registration', function(e){
     e.preventDefault();
     $(".documents-list").html("");
@@ -287,23 +270,24 @@ $(document).on('click', '.documents-title-icon-registration', function(e){
     $('.documents-dropdown').fadeOut(50);
     $('.documents-select').fadeOut(50);
 
-
     DocEndtitle = $(this).data('title')
     DocEndtext = $(this).data('text')
     DocEndid = $(this).data('id')
     DocEndcitizenid = $(this).data('csn')
 
-    var AddOption = '<div class="document-body-class-body-main">'+
-                        '<div id="documents-textarea-new" spellcheck="false" required placeholder="Text" maxlength="4000">'+DocEndtext+'</div>'+
-                    '</div>';
+    var AddOption = `
+    <div class="document-body-class-body-main">
+        <div id="documents-textarea-new" spellcheck="false" required placeholder="Text" maxlength="4000">${DocEndtext}</div>
+    </div>`;
 
-    var AnotherOption = '<div class="document-body-class-body-main">'+
-                            '<div class="documents-extras-button-registration"><i class="fas fa-ellipsis-v"></i></div>'+
-                            '<div class="documents-input-title-list">Title</div>'+
-                            '<div class="documents-input-title-name">'+DocEndtitle+'</div>'+
-                            '<div class="documents-input-tags"><i class="fas fa-tags"></i></div>'+
-                            '<div class="documents-input-back"><i class="fas fa-chevron-left"></i></div>'+
-                        '</div>';
+    var AnotherOption = `
+    <div class="document-body-class-body-main">
+        <div class="documents-extras-button-registration"><i class="fas fa-ellipsis-v"></i></div>
+        <div class="documents-input-title-list">Title</div>
+        <div class="documents-input-title-name">${DocEndtitle}</div>'+
+        <div class="documents-input-tags"><i class="fas fa-tags"></i></div>'+
+        <div class="documents-input-back"><i class="fas fa-chevron-left"></i></div>'+
+    </div>`;
 
     $('.documents-list').append(AddOption);
     $('.documents-header').append(AnotherOption);
@@ -343,17 +327,19 @@ $(document).on('click', '.documents-title-icon', function(e){
     DocEndid = $(this).data('id')
     DocEndcitizenid = $(this).data('csn')
 
-    var AddOption = '<div class="document-body-class-body-main">'+
-                        '<textarea id="documents-textarea-new" spellcheck="false" required placeholder="Text" maxlength="4000">'+DocEndtext+'</textarea>'+
-                    '</div>';
+    var AddOption = `
+    <div class="document-body-class-body-main">
+        <textarea id="documents-textarea-new" spellcheck="false" required placeholder="Text" maxlength="4000">${DocEndtext}</textarea>
+    </div>`;
 
-    var AnotherOption = '<div class="document-body-class-body-main">'+
-                            '<div class="documents-extras-button"><i class="fas fa-ellipsis-v"></i></div>'+
-                            '<div class="documents-input-title-list">Title</div>'+
-                            '<div class="documents-input-title-name">'+DocEndtitle+'</div>'+
-                            '<div class="documents-input-tags"><i class="fas fa-tags"></i></div>'+
-                            '<div class="documents-input-back"><i class="fas fa-chevron-left"></i></div>'+
-                        '</div>';
+    var AnotherOption = `
+    <div class="document-body-class-body-main">
+        <div class="documents-extras-button"><i class="fas fa-ellipsis-v"></i></div>
+        <div class="documents-input-title-list">Title</div>
+        <div class="documents-input-title-name">${DocEndtitle}</div>
+        <div class="documents-input-tags"><i class="fas fa-tags"></i></div>
+        <div class="documents-input-back"><i class="fas fa-chevron-left"></i></div>
+    </div>`;
 
     $('.documents-list').append(AddOption);
     $('.documents-header').append(AnotherOption);
